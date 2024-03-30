3afcd1e0cebf1323168c96288ccb7486e5f23340
markohautala <markohautala@live.se>
2024-03-30 12:44:39 +0100

    Update settings and add new routes

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")  # This is the home route
-def index():             # This is the function tied to the home route
-    return "Hello, World!"  # This is the response
+def index():             # This is the view tied to the home route
+    return render_template("index.html") # This will render the index.html template
+
+@app.route("/about") # This is the about route
+def about():  #This is a view 
+    return render_template("about.html") # This will render the about.html template
+
+@app.route("/contact") # This is the about route
+def contact():  #This is a view 
+    return render_template("contact.html") # This will render the about.html template
 
 if __name__ == "__main__":
     app.run(
