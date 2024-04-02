9fde08a06d229928d829de552e8103ac304d59fd
markohautala <markohautala@live.se>
2024-04-02 13:13:32 +0200

    Change About page to Inspiration

--- app.py
+++ app.py
@@ -8,19 +8,19 @@ def index():             # This is the view tied to the home route
     return render_template("index.html") # This will render the index.html template
 
 
-@app.route("/about") # This is the about route
-def about():  #This is a view 
-    return render_template("about.html") # This will render the about.html template
+@app.route("/inspiration") # This is the about route
+def inspiration():  #This is a view 
+    return render_template("inspiration.html", page_title="Inspiration") # This will render the about.html template
 
 
 @app.route("/contact") # This is the about route
 def contact():  #This is a view 
-    return render_template("contact.html") # This will render the about.html template
+    return render_template("contact.html", page_title="Contact") # This will render the about.html template
 
 
 @app.route("/careers") # This is the about route
 def careers():  #This is a view 
-    return render_template("careers.html") # This will render the about.html template
+    return render_template("careers.html", page_title="Careers") # This will render the about.html template
 
 
 if __name__ == "__main__":
