ff429746c49ee7410476c839616def6dddc7fe0c
markohautala <markohautala@live.se>
2024-04-04 23:30:10 +0200

    Change debugger to False

--- app.py
+++ app.py
@@ -37,5 +37,5 @@ if __name__ == "__main__":
     app.run(
         host=os.getenv("IP", "localhost"), # This allows the app to run on any IP address
         port=int(os.getenv("PORT", 5000)), #5000 is the default port for Flask
-        debug=True # This will allow for debugging messages in the console
+        debug=False
     )
